217ff6715db511fdbb4aa00b515fe350ed7874d6
glehemaa <146538213+glehemaa@users.noreply.github.com>
2023-10-28 11:32:39 +0300

    Create checkTemp.js

---
+++ checkTemp.js
@@ -0,0 +1,5 @@
+function checkTemp (temperature) {
+if ( temperature >= 0 && temperature <= 20) { return -1 } 
+    else if ( temperature >= 21 && temperature <= 40) { return 0 }
+        else if ( temperature >= 41 && temperature <= 60) { return 1 }
+}
